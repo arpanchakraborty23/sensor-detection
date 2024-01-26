4785308c32a0c910f025c0915e35f0b52de12fdb
Arpan <arpanchakraborty500@gmail.com>
2024-01-26 19:26:47 +0530

    commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="AC",
+    author_email="arpanchakraborty500@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
